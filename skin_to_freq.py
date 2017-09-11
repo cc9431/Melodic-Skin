8a38aef3a3781e2ff39224097ce28178fbdc4447
Charles Calder <cc9431@bard.edu>
2017-09-11 19:35:25 -0400

    Class update

--- skin_to_freq.py
+++ skin_to_freq.py
@@ -21,8 +21,7 @@ class TonePlayer(object):
 
     def pixel_to_freq(self, pixel):
         '''Given rgb values, convert those into a frequency on the audible spectrum'''
-        (r, g, b) = pixel[1]
-        return colorsys.rgb_to_hsv(r, g, b)
+        return colorsys.rgb_to_hsv(pixel[1][0], pixel[1][1], pixel[1][2])
 
 
     def freq_to_tone(self, freq, amplitude=1):
@@ -41,6 +40,12 @@ class TonePlayer(object):
         sin = (math.sin(frame / ((self.bitrate / freq) / math.pi)) * 127 + 128)
         return chr(int(amplitude * sin))
 
+    def test_scale(self):
+        '''play a scale to test pyaudio'''
+        scale = [130.8, 146.8, 164.8, 174.6, 195.0, 220.0, 246.9, 261.6]
+        for freq in scale:
+            self.freq_to_tone(freq)
+
     def play(self):
         '''Play a tone given a stream of data'''
         self.stream = self.pa.open(
@@ -52,94 +57,92 @@ class TonePlayer(object):
         self.stream.stop_stream()
         self.stream.close()
 
-def swap(tuple_array, i, j):
-    tuple_array[i], tuple_array[j] = tuple_array[j], tuple_array[i]
+class Portrait(object):
+    '''Class for converting images into usable tuple arrays of (count, color)'''
 
-def heapify(tuple_array, end, i):
-    l = 2 * i + 1
-    r = 2 * (i + 1)
-    max_num = i
-    if l < end and tuple_array[i] < tuple_array[l]:
-        max_num = l
-    if r < end and tuple_array[max_num] < tuple_array[r]:
-        max_num = r
-    if max_num != i:
-        swap(tuple_array, i, max_num)
-        heapify(tuple_array, end, max_num)
+    def __init__(self, file_path, save_path, array_length=50, thresh=110, col_size=20):
+        self.path_to_file = file_path
+        self.save_to_file = save_path
+        self.threshold = thresh
+        self.img = Image.open(self.path_to_file)
+        self.height, self.width = self.img.size
+        self.tuple_array = self.img.getcolors(self.height * self.width)
+        self.color_size = col_size
+        self.length = array_length
 
-def keyvalue_heap_sort(tuple_array):
-    last = len(tuple_array)
-    first = last // 2 - 1
-    for i in range(first, -1, -1):
-        heapify(tuple_array, last, i)
-    for i in range(last-1, 0, -1):
-        swap(tuple_array, i, 0)
-        heapify(tuple_array, i, 0)
+    def analyze(self, resz=False, save=False):
+        '''Sorts pixels and return a list of the most common non-white pixels'''
+        self.heap_sort()
+        self.remove_white()
+        if resz:
+            self.resize()
+        if save:
+            self.draw_and_save()
 
-def remove_white(array, threshold=110):
-    '''removes any pixel with a color that is similair to the most common color'''
-    top_pixel = array[-1]
-    top_color = top_pixel[1]
-    #top_color = (190, 140, 110)
-    new_array = [(0, 0)]
-    for (cnt, clr) in array:
-        add_item = False
-        for i in xrange(3):
-            add_item = add_item or (abs(top_color[i] - clr[i]) > threshold)
-        if add_item:
-            new_array.append((cnt, clr))
-    return new_array
+    def remove_white(self):
+        '''removes any pixel with a color that is similair to the most common color'''
+        top_pixel = self.tuple_array[-1]
+        top_color = top_pixel[1]
+        # jazz_color = (190, 140, 110)
+        new_array = [(0, 0)]
+        for (cnt, clr) in self.tuple_array:
+            add_item = False
+            for i in xrange(3):
+                add_item = add_item or (abs(top_color[i] - clr[i]) > self.threshold)
+            if add_item:
+                new_array.append((cnt, clr))
+        self.tuple_array = new_array
 
-def find_common_colors(img):
-    '''Given the file name of an image, open it, sort its pixels
-    and return a list of the most common non-white pixels'''
-    width, height = img.size
-    pxls = img.getcolors(width * height)
+    def draw_and_save(self):
+        '''(Meant for testing) Draws the most commonly found colors in an image onto an image'''
+        draw = ImageDraw.Draw(self.img)
+        length = self.height/self.color_size
+        if  len(self.tuple_array) < length:
+            length = len(self.tuple_array)
+        for item in xrange(length):
+            count_color = self.tuple_array[item]
+            x_y = [0, item * self.color_size, 300, item * self.color_size + self.color_size]
+            draw.rectangle(x_y, count_color[1], (0, 0, 0))
+        del draw
+        self.img.save(self.save_to_file)
 
-    most_common_pixels = []
+    def resize(self):
+        '''Resize our tuple array'''
+        self.tuple_array = [self.tuple_array[-x-1] for x in xrange(self.length)]
 
-    keyvalue_heap_sort(pxls)
-    pixels = remove_white(pxls)
+    def swap(self, i, j):
+        self.tuple_array[i], self.tuple_array[j] = self.tuple_array[j], self.tuple_array[i]
 
-    for pix in xrange(len(pixels)/2):
-        col_count = pixels[-pix-1]
-        col = col_count[1]
-        most_common_pixels.append(pixels[-pix-1])
+    def heapify(self, end, i):
+        l = 2 * i + 1
+        r = 2 * (i + 1)
+        max_num = i
+        if l < end and self.tuple_array[i] < self.tuple_array[l]:
+            max_num = l
+        if r < end and self.tuple_array[max_num] < self.tuple_array[r]:
+            max_num = r
+        if max_num != i:
+            self.swap(i, max_num)
+            self.heapify(end, max_num)
 
-    return most_common_pixels
+    def heap_sort(self):
+        last = len(self.tuple_array)
+        first = last // 2 - 1
+        for i in range(first, -1, -1):
+            self.heapify(last, i)
+        for i in range(last-1, 0, -1):
+            self.swap(i, 0)
+            self.heapify(i, 0)
 
-def test_pixel_count():
-    '''Test function to make sure pixel counting works'''
-    im = Image.open("TestFiles/test_jasmine.jpg")
-    awesomelist = find_common_colors(im)
-    draw = ImageDraw.Draw(im)
-    tp = TonePlayer()
-    color_size = 20
+def test(filepath, savepath):
+    '''Function to test that the pyaudio and color analysis sides work as they should'''
+    tone = TonePlayer()
 
-    print len(awesomelist)
+    port = Portrait(filepath, savepath)
+    port.analyze(True, True)
 
-    for item in xrange(im.height/color_size):
-        if item < len(awesomelist):
-            color_count = awesomelist[item]
-            x_y = [0, item * color_size, 300, item * color_size + color_size]
-            draw.rectangle(x_y, color_count[1], (0, 0, 0))
-            print color_count[1], tp.pixel_to_freq(color_count) # print hsv value
-    del draw
+    for cnt_col in port.tuple_array:
+        print cnt_col, tone.pixel_to_freq(cnt_col)
 
-    im.save("TestFiles/test_no_bkgn_result2.jpg")
 
-test_pixel_count()
-
-def test_audio():
-    '''Function to test that the pyaudio side works as it should'''
-    #scale = [130.8, 146.8, 164.8, 174.6, 195.0, 220.0, 246.9, 261.6]
-    scale2 = [400, 800, 500, 900, 700, 800]
-
-    tp = TonePlayer()
-
-    for freq in scale2:
-        tp.freq_to_tone(freq)
-
-    tp.play()
-
-    tp.pa.terminate()
+test("TestFiles/test_obama.jpg", "TestFiles/test_obama_result.jpg")
