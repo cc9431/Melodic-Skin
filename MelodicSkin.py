a92346de9a63fe7850342e5cf4d9ccc0f0f21a27
Charles Calder <cc9431@bard.edu>
2017-09-12 20:49:58 -0400

    minheapsort ready

--- MelodicSkin.py
+++ MelodicSkin.py
@@ -1,8 +1,9 @@
 '''Testing out code for a friend's art project about turning skin tone into sound'''
-# Charlie Calder <cc9431@bard.edu>
+# Charles Calder <cc9431@bard.edu>
 # September 7th 2017
 
 import colorsys
+import sys
 import math
 from PIL import Image, ImageDraw
 import pyaudio
@@ -10,24 +11,23 @@ import pyaudio
 class TonePlayer(object):
     '''Class for converting lists of color into frequencies,
     playing them, and saving them as wav files'''
-
-    def __init__(self, length=0.2, bitrate=44100, maxFreq=2093.00, minFreq=130.81):
+    MAXFREQ = 2093.00
+    MINFREQ = 130.81
+    BITRATE = 44100
+    DURATION = 0.2
+    def __init__(self):
         self.pa = pyaudio.PyAudio()
-        self.bitrate = bitrate
-        self.length = length
-        self.maxFreq = maxFreq
-        self.minFreq = minFreq
         self.data = ''
 
     def pixel_to_freq(self, pixel):
         '''Given rgb values, convert those into a frequency on the audible spectrum'''
-        return colorsys.rgb_to_hsv(pixel[1][0], pixel[1][1], pixel[1][2])
+        return colorsys.rgb_to_hls(pixel[1][0], pixel[1][1], pixel[1][2])
 
 
     def freq_to_tone(self, freq, amplitude=1):
         '''Play a tone given a certain frequency'''
         # Frequency needs to be between 130.81 - 2093.00 # 261.63 = C4-note.
-        numOfFrames = int(self.bitrate * self.length)
+        numOfFrames = int(self.BITRATE * self.DURATION)
         data = ''
 
         for x in xrange(numOfFrames):
@@ -37,7 +37,7 @@ class TonePlayer(object):
 
     def sin_wave(self, freq, frame, amplitude):
         '''Convert a frequency/frame/amplitude into a character for our data stream'''
-        sin = (math.sin(frame / ((self.bitrate / freq) / math.pi)) * 127 + 128)
+        sin = (math.sin(frame / ((self.BITRATE / freq) / math.pi)) * 127 + 128)
         return chr(int(amplitude * sin))
 
     def test_scale(self):
@@ -51,7 +51,7 @@ class TonePlayer(object):
         self.stream = self.pa.open(
             format=self.pa.get_format_from_width(1),
             channels=1,
-            rate=self.bitrate,
+            rate=self.BITRATE,
             output=True)
         self.stream.write(self.data)
         self.stream.stop_stream()
@@ -59,8 +59,7 @@ class TonePlayer(object):
 
 class Portrait(object):
     '''Class for converting images into usable tuple arrays of (count, color)'''
-
-    def __init__(self, file_path, save_path, array_length=50, thresh=110, col_size=20):
+    def __init__(self, file_path, save_path, array_length=50, thresh=100, col_size=20):
         self.path_to_file = file_path
         self.save_to_file = save_path
         self.threshold = thresh
@@ -72,7 +71,7 @@ class Portrait(object):
 
     def analyze(self, resz=False, save=False):
         '''Sorts pixels and return a list of the most common non-white pixels'''
-        self.heap_sort()
+        self.min_heap_sort()
         self.remove_white()
         if resz:
             self.resize()
@@ -81,10 +80,9 @@ class Portrait(object):
 
     def remove_white(self):
         '''removes any pixel with a color that is similair to the most common color'''
-        top_pixel = self.tuple_array[-1]
-        top_color = top_pixel[1]
-        # jazz_color = (190, 140, 110)
-        new_array = [(0, 0)]
+        top_color = self.tuple_array[0][1]
+        #print top_color
+        new_array = []
         for (cnt, clr) in self.tuple_array:
             add_item = False
             for i in xrange(3):
@@ -102,47 +100,52 @@ class Portrait(object):
         for item in xrange(length):
             count_color = self.tuple_array[item]
             x_y = [0, item * self.color_size, 300, item * self.color_size + self.color_size]
-            draw.rectangle(x_y, count_color[1], (0, 0, 0))
+            draw.rectangle(x_y, count_color[1])#, (0, 0, 0))
         del draw
         self.img.save(self.save_to_file)
 
     def resize(self):
         '''Resize our tuple array'''
-        self.tuple_array = [self.tuple_array[-x-1] for x in xrange(self.length)]
+        self.tuple_array = [self.tuple_array[x] for x in xrange(self.length)]
 
     def swap(self, i, j):
+        '''Swap two values given thier position in the array'''
         self.tuple_array[i], self.tuple_array[j] = self.tuple_array[j], self.tuple_array[i]
 
     def heapify(self, end, i):
-        l = 2 * i + 1
-        r = 2 * (i + 1)
-        max_num = i
-        if l < end and self.tuple_array[i] < self.tuple_array[l]:
-            max_num = l
-        if r < end and self.tuple_array[max_num] < self.tuple_array[r]:
-            max_num = r
-        if max_num != i:
-            self.swap(i, max_num)
-            self.heapify(end, max_num)
+        '''Given end of list and position value, place the value into the heap'''
+        left = 2 * i
+        right = (2 * i) + 1
+        min_num = i
+        if left < end and self.tuple_array[i] > self.tuple_array[left]:
+            min_num = left
+        if right < end and self.tuple_array[min_num] > self.tuple_array[right]:
+            min_num = right
+        if min_num != i:
+            self.swap(i, min_num)
+            self.heapify(end, min_num)
 
-    def heap_sort(self):
-        last = len(self.tuple_array)
-        first = last // 2 - 1
+    def min_heap_sort(self):
+        '''heap sort with a min-heap, to sort the pixel array with the max number at the front'''
+        last = len(self.tuple_array) - 1
+        first = (last // 2)
         for i in range(first, -1, -1):
             self.heapify(last, i)
-        for i in range(last-1, 0, -1):
+        for i in range(last, -1, -1):
             self.swap(i, 0)
             self.heapify(i, 0)
 
-def test(filepath, savepath):
+def test(filepath):
     '''Function to test that the pyaudio and color analysis sides work as they should'''
-    tone = TonePlayer()
+    filepath_minus_extension = filepath[:-4]
+    savepath = filepath_minus_extension + "_result.jpg"
 
-    port = Portrait(filepath, savepath)
+    port = Portrait(filepath, savepath, 150, 120)
     port.analyze(True, True)
 
-    for cnt_col in port.tuple_array:
-        print cnt_col, tone.pixel_to_freq(cnt_col)
+    #tone = TonePlayer()
+    #for cnt_col in port.tuple_array:
+    #    print cnt_col, tone.pixel_to_freq(cnt_col)
 
-
-test("TestFiles/test_obama.jpg", "TestFiles/test_obama_result.jpg")
+if __name__ == "__main__":
+    test(str(sys.argv[1]))
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-## This is part of an art project that I am helping a friend to create.
+# Melodic Skin
 
+required packages: pyaudio, Pillow, math, colorsys
+To run: type 'python MelodicSkin.py [name of your file]'
+(only run this on .jpg or .png files)
+
+### This is part of an art project that I am helping a friend to create. 
 The conversation around race is a difficult but necessary one. People are treated differently based on their what they look like, but when a computer takes our image and breaks it down into pixels and RBG values, we are all simply strings of ones and zeros.
 This project is designed to take an image of a person, – preferably with a white* background and white* clothing, in order to isolate their tones – find the most common non-white* colors, and turn those tones into a melody. Each person’s skin, hair, and eyes will contribute to a unique melody.
 
